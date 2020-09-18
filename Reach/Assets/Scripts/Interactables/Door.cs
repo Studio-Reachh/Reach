41da832c2f8ded00033480320791cd79368d307b
nienkeov <nm.overmeer@gmail.com>
2020-09-18 10:11:58 +0200

    added lock on door gameobject, added all sprites for room02, fixed vibrate bug

--- Reach/Assets/Scripts/Interactables/Door.cs
+++ Reach/Assets/Scripts/Interactables/Door.cs
@@ -1,28 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : Interactable
 {
     private LevelLoader _levelLoader;
+
+    [Header("Load Next Level")]
     public string LevelName;
 
-    public override bool Interact(Item item)
-    {
-        throw new System.NotImplementedException();
-    }
+    [Header("Unlock Door")]
+    public bool IsDoorLocked;
+    public string KeyGameobject;
+    public string KeyProperty;
 
     private void Awake()
     {
         _levelLoader = GameObject.Find("LevelLoader").GetComponent<LevelLoader>();
     }
 
+    public override bool Interact(Item item)
+    {
+        return true;
+    }
+
     private void OnMouseDown()
     {
-        if(LevelName != string.Empty)
+        if (IsDoorLocked)
         {
-            _levelLoader.LoadNextLevel(LevelName);
-            Interact(null);
+            if (SaveHandler.GetValueByProperty(SceneManager.GetActiveScene().name, KeyGameobject, KeyProperty, out bool isUnlocked ))
+            {
+                if (LevelName != string.Empty)
+                {
+                    _levelLoader.LoadNextLevel(LevelName);
+                    Interact(null);
+                }
+            }
+        } else
+        {
+            if (LevelName != string.Empty)
+            {
+                _levelLoader.LoadNextLevel(LevelName);
+                Interact(null);
+            }
         }
     }
 }
