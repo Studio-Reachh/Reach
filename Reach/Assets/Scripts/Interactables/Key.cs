db70d60e2367e55843899b0bdf4f73ea7d77dc3c
nienkeov <nm.overmeer@gmail.com>
2020-10-01 10:20:50 +0200

    implemented gameplay room09

---
+++ Reach/Assets/Scripts/Interactables/Key.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    private Vector2 _movementOnAxis;
+    private bool _hasKeyFallen = false;
+    private float _slideBackKey;
+
+    public float SlideBackPosX = 2;
+
+    public float Speed;
+
+    private void Start()
+    {
+        _rigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (_hasKeyFallen)
+        {
+            _movementOnAxis = new Vector2(Input.acceleration.x, 0);
+        } 
+        else
+        {
+            _movementOnAxis = new Vector2(Input.acceleration.x, Input.acceleration.y);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "floor")
+        {
+            _hasKeyFallen = true;
+        }
+
+        if(collision.gameObject.tag == "Barrier")
+        {
+            var topOfBarrier = collision.contacts[0].normal;
+
+            if (topOfBarrier.y < 0)
+            {
+                _slideBackKey = transform.position.x - SlideBackPosX;
+                transform.position = new Vector2(_slideBackKey, transform.position.y);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = _movementOnAxis * Speed * Time.fixedDeltaTime;
+    }
+}
---
+++ Reach/Assets/Scripts/Interactables/LeverBox.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class LeverBox : Interactable
+{
+    private bool _isLeverUp = true;
+
+    [Header("sprites")]
+    public Sprite LeverupSprite;
+    public Sprite LeverDownSprite;
+
+    [Header("Barrier Y Axis")]
+    public float YPositionBarrierUp;
+    public float YPositionBarrierDown;
+    public float Speed;
+
+    private GameObject[] _barriers;
+
+    private void Awake()
+    {
+        _barriers = GameObject.FindGameObjectsWithTag("Barrier");
+    }
+    public override bool Interact(Item item)
+    {
+        bool succesfullInteraction = false;
+
+        if (_isLeverUp)
+        {
+            _isLeverUp = false;
+        } else
+        {
+            _isLeverUp = true;
+        }
+
+        return succesfullInteraction;
+    }
+
+    private void Leverup()
+    {
+        foreach (var barrier in _barriers)
+        {
+            if (barrier.name == "barrierUp")
+            {
+                barrier.transform.position = Vector2.MoveTowards(barrier.transform.position, new Vector2(barrier.transform.position.x, YPositionBarrierUp), Speed * Time.fixedDeltaTime); 
+            }
+            else
+            {
+                barrier.transform.position = Vector2.MoveTowards(barrier.transform.position, new Vector2(barrier.transform.position.x, YPositionBarrierDown), Speed * Time.fixedDeltaTime);
+            }
+        }
+    }
+
+    private void LeverDown()
+    {
+        foreach (var barrier in _barriers)
+        {
+            if (barrier.name == "barrierUp")
+            {
+                barrier.transform.position = Vector2.MoveTowards(barrier.transform.position, new Vector2(barrier.transform.position.x, YPositionBarrierDown), Speed * Time.fixedDeltaTime);
+            }
+            else
+            {
+                barrier.transform.position = Vector2.MoveTowards(barrier.transform.position, new Vector2(barrier.transform.position.x, YPositionBarrierUp), Speed * Time.fixedDeltaTime);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (_isLeverUp)
+        {
+            Leverup();
+        } else
+        {
+            LeverDown();
+        }
+    }
+}
